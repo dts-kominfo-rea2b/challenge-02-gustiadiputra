5434b504a0154745b93b0955e7066bec61ef6163
Gusti Adi Putra <gustiadi.putra20@gmail.com>
2022-05-28 12:00:21 +0700

    Challenge logika done

--- index.js
+++ index.js
@@ -23,52 +23,74 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+      let hasilLooping = [];
+      for (hasilLooping=0; hasilLooping<dataYangAkanDilooping.length; hasilLooping++){
+        console.log(dataYangAkanDilooping[hasilLooping].namaDepan + " " + dataYangAkanDilooping[hasilLooping].namaBelakang);
+      }
 
-  /*
-    TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
-      yang berisi jumlah pria dari masing masing pegawai
-  */
-  let jumlahPria = null;
-
-  /*
-    TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
-      yang berisi jumlah wanita dari masing masing pegawai
-  */
-  let jumlahWanita = null;
-
-  /*
-    TODO 4: Buatlah sebuah variabel bernama "komentar"
-      yang akan mengomentari apakah lebih banyak Pria atau Wanita
       
-      Apabila Pria lebih banyak dari Wanita, maka komentar akan berisi:
-        "Jumlah Pria lebih banyak dari Wanita"
-      Apabila Wanita lebih banyak dari Pria, maka komentar akan berisi:
-        "Jumlah Wanita lebih banyak dari Pria"
-      Apabila imbang, maka komentar akan berisi:
-        "Jumlah Pria dan Wanita berimbang"
-  */
-  let komentar = null;
+      /*
+        TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
+          yang berisi jumlah pria dari masing masing pegawai
+      */
+      let jumlahPria = 0;
+      for (hasilLooping in dataYangAkanDilooping){
+        if(dataYangAkanDilooping[hasilLooping].jenisKelamin == "M"){
+          jumlahPria++
+        }  
+      }
+      
 
-  // ! JANGAN DIMODIFIKASI
-  return {
-    hasilLooping,
-    jumlahPria,
-    jumlahWanita,
-    komentar,
-  };
-}
-
-function main(data) {
-  const hasil = lakukanLooping(data || arrayObjectPegawai);
-
-  console.log(hasil.hasilLooping);
-  console.log(hasil.jumlahPria);
-  console.log(hasil.jumlahWanita);
-
-  return hasil;
-}
-
-main(arrayObjectPegawai);
-
-module.exports = main;
+      /*
+        TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
+          yang berisi jumlah wanita dari masing masing pegawai
+      */
+      let jumlahWanita = 0;
+      for (hasilLooping in dataYangAkanDilooping){
+        if(dataYangAkanDilooping[hasilLooping].jenisKelamin == "F"){
+          jumlahWanita++
+        }
+      }
+      /*
+        TODO 4: Buatlah sebuah variabel bernama "komentar"
+          yang akan mengomentari apakah lebih banyak Pria atau Wanita
+          
+          Apabila Pria lebih banyak dari Wanita, maka komentar akan berisi:
+            "Jumlah Pria lebih banyak dari Wanita"
+          Apabila Wanita lebih banyak dari Pria, maka komentar akan berisi:
+            "Jumlah Wanita lebih banyak dari Pria"
+          Apabila imbang, maka komentar akan berisi:
+            "Jumlah Pria dan Wanita berimbang"
+      */
+      let komentar = "";
+      if (jumlahPria > jumlahWanita){
+        komentar =" Jumlah Pria Lebih banyak dari Wanita"
+      } else if ( jumlahPria < jumlahWanita){
+        komentar ="Jumlah Wanita lebih banyak dari Pria"
+      } else {
+        komentar ="Jumlah Pria dan Wanita berimbang"
+      }
+     
+    
+      // ! JANGAN DIMODIFIKASI
+      return {
+        hasilLooping,
+        jumlahPria,
+        jumlahWanita,
+        komentar,
+      };
+    }
+    
+    function main(data) {
+      const hasil = lakukanLooping(data || arrayObjectPegawai);
+    
+      console.log(hasil.hasilLooping);
+      console.log(hasil.jumlahPria);
+      console.log(hasil.jumlahWanita);
+    
+      return hasil;
+    }
+    
+    main(arrayObjectPegawai);
+    
+    module.exports = main;
